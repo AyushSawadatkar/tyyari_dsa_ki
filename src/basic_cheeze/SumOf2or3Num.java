cff3da1aed22e0bd74c2ad85387d333d7cbb45ef
ayush Sawadatkar <aayushsawadatkar@gmail.com>
2026-02-18 13:39:25 +0530

    day 2

---
+++ src/basic_cheeze/SumOf2or3Num.java
@@ -0,0 +1,22 @@
+package basic_cheeze;
+
+import java.util.Scanner;
+
+public class SumOf2or3Num {
+    static void main() {
+        Scanner sc = new Scanner(System.in);//input lene keliye
+        System.out.print("enter the num1 :");
+        int a = sc.nextInt();
+
+        System.out.print("enter the num2 :");
+        int b = sc.nextInt();
+
+        System.out.print("Enter the num3 :");
+        int c = sc.nextInt();
+        int d = a * b * c;
+
+        System.out.print("Answer is :");
+        System.out.print(d);
+
+    }
+}
