e79ea213e5c38965a19c77b74bfb54adfc220565
ayush Sawadatkar <aayushsawadatkar@gmail.com>
2026-02-18 13:16:05 +0530

    day 2

--- src/basic_cheeze/UserInput.java
+++ src/basic_cheeze/UserInput.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class UserInput {
     static void main() {
         Scanner sc = new Scanner(System.in);// input lene keliye
-        System.out.println("Enter the raduis:");
+        System.out.print("Enter the raduis:");
         double r = sc.nextDouble();
         double a = 3.1415 * r * r;
         System.out.println(a);
