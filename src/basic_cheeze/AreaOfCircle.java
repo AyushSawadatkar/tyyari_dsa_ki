49348837da0f7d96f9a2d1d9a5ca8454a01ecabc
ayush Sawadatkar <aayushsawadatkar@gmail.com>
2026-02-18 11:20:11 +0530

    day 2

---
+++ src/basic_cheeze/AreaOfCircle.java
@@ -0,0 +1,13 @@
+package basic_cheeze;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class AreaOfCircle {
+    static void main() {
+        //area kya hota hai? pi r square
+        double r = 3.8;
+        double a = 3.141592 * r * r ;
+        System.out.println(a);
+
+    }
+}
