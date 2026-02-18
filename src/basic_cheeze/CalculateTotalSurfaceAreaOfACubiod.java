85a93f87c09ce6c53136507607459146ba332d37
ayush Sawadatkar <aayushsawadatkar@gmail.com>
2026-02-18 11:37:29 +0530

    day 2

---
+++ src/basic_cheeze/CalculateTotalSurfaceAreaOfACubiod.java
@@ -0,0 +1,11 @@
+package basic_cheeze;
+
+public class CalculateTotalSurfaceAreaOfACubiod {
+    static void main() {
+        double l = 4.444;
+        double b = 6.8;
+        double h = 999;
+        double TSAOAC = 2 * (l*b+b*h+l*h);
+        System.out.println(TSAOAC);
+    }
+}
--- src/basic_cheeze/CalculateVolumeOfSphere.java
+++ src/basic_cheeze/CalculateVolumeOfSphere.java
@@ -2,7 +2,7 @@ package basic_cheeze;
 
 public class CalculateVolumeOfSphere {
     static void main() {
-        double r = 5;
+        double r = 5.444;
         double v = 4/3 * 3.1415 * r * r * r;
         System.out.println(v);
     }
