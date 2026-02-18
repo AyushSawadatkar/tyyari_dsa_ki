71b87c49127aa3286f227ee2b4d7ac733c9a5469
ayush Sawadatkar <aayushsawadatkar@gmail.com>
2026-02-18 10:48:24 +0530

    day 2

---
+++ src/basic_cheeze/ArithmeticOperationsOnInt.java
@@ -0,0 +1,12 @@
+package basic_cheeze;
+
+public class ArithmeticOperationsOnInt {
+    static void main() {
+        float x = 5 , y = 6;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x%y);
+        System.out.println(x/y);
+    }
+}
