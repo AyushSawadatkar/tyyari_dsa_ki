76b55f934bce631e72106e621ff57b377ee5746a
ayush Sawadatkar <aayushsawadatkar@gmail.com>
2026-02-18 16:56:25 +0530

    day 2

--- src/basic_cheeze/CalculateTotalSurfaceAreaOfACubiod.java
+++ src/basic_cheeze/CalculateTotalSurfaceAreaOfACubiod.java
@@ -4,7 +4,7 @@ public class CalculateTotalSurfaceAreaOfACubiod {
     static void main() {
         double l = 4.444;
         double b = 6.8;
-        double h = 999;
+        double h = 99;
         double TSAOAC = 2 * (l*b+b*h+l*h);
         System.out.println(TSAOAC);
     }
