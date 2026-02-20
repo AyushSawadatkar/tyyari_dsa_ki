7d34cdbf31d2e6dec1322e911043b1bbb6f076d0
ayush Sawadatkar <aayushsawadatkar@gmail.com>
2026-02-20 15:04:19 +0530

    day 3

---
+++ src/basic_cheeze/IncrementDecrement.java
@@ -0,0 +1,35 @@
+package basic_cheeze;
+
+public class IncrementDecrement {
+    static void main() {
+        int x = 3;
+        System.out.println(x);
+        x += 4;//x = x + 4
+        System.out.println(x);
+        x -= 5;
+        System.out.println(x);
+        x *= 8;
+        System.out.println(x);
+        x /= 4;
+        System.out.println(x);
+
+        int y = 7 ;
+        System.out.println(y);
+        y++; // y = y + 1
+        // post increment
+        // phle use karo then badhaoo
+        System.out.println(y);
+        y--;
+        System.out.println(y);
+
+        int z =7;
+        System.out.println(z);
+        ++z; // z = z + 1
+        // pre increment
+        // phle badhoo then use karo
+        System.out.println(z);
+        z--;
+        System.out.println(z);
+
+    }
+}
