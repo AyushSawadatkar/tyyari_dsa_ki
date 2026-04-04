b9d15983898755466b5166f8574c8733693d6486
ayush Sawadatkar <aayushsawadatkar@gmail.com>
2026-04-04 14:14:22 +0530

    day 4

---
+++ src/if_else/MagnitudeSmallerThan69orNot.java
@@ -0,0 +1,13 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class MagnitudeSmallerThan69orNot {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number :");
+        int n = sc.nextInt();
+        if(n<69) System.out.println(" magnitude smaller than 69");
+        else System.out.println(" magnitude not smaller than 69");
+    }
+}
