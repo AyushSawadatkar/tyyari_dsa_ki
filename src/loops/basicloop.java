8e93cf84b655aa8dd33e8649395535c028cf8d31
ayush Sawadatkar <aayushsawadatkar@gmail.com>
2026-04-06 19:17:47 +0530

    day 5

---
+++ src/loops/basicloop.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class basicloop {
+    static void main() {
+         for(int i=1;i<=100;i++) {
+             System.out.println("advait bestfriend is ishika and malhar");
+
+         }
+    }
+}
