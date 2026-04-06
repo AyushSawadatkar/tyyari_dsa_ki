ae2cebd49ae6fa0a1744ef99e5448e2039f7fbd1
ayush Sawadatkar <aayushsawadatkar@gmail.com>
2026-04-06 20:56:54 +0530

    day 5

---
+++ src/loops/OneToTen.java
@@ -0,0 +1,9 @@
+package loops;
+
+public class OneToTen {
+    static void main() {
+        for( int i = 1; i<=10 ; i++ ) {
+            System.out.println(i+" ");
+        }
+    }
+}
--- src/loops/basicloop.java
+++ src/loops/basicloop.java
@@ -2,7 +2,7 @@ package loops;
 
 public class basicloop {
     static void main() {
-         for(int i=1;i<=100;i++) {
+         for(int i=-99;i<=100;i++) {
              System.out.println("advait bestfriend is ishika and malhar");
 
          }
