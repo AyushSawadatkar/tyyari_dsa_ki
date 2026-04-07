14e269f4c006886ae46f40905ede766d71d29ad7
ayush Sawadatkar <aayushsawadatkar@gmail.com>
2026-04-07 18:07:39 +0530

    day 5

---
+++ src/loops/NTimesPrint.java
@@ -0,0 +1,15 @@
+package loops;
+
+import java.util.Scanner;
+
+public class NTimesPrint {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number :");
+        int n = sc.nextInt();
+        for (int i = 0; i <=n ; i++) {
+            System.out.println("arjun is brave warrior");
+
+        }
+    }
+}
